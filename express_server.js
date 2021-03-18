908b54cb1e2233f6c41ed632945b23eed5d16c0f
Calvin Chan <calvinchan87@gmail.com>
2021-03-18 04:31:25 +0000

    Allow edit exsting shortened URLs

--- express_server.js
+++ express_server.js
@@ -56,6 +56,16 @@ app.get("/urls/:shortURL", (req, res) => {
   res.render("urls_show", templateVars);
 });
 
+app.post("/urls/:shortURL/delete", (req, res) => {
+  delete urlDatabase[req.params.shortURL]; // Edward Smith RA
+  res.redirect('/urls/');
+});
+
+app.post("/urls/:shortURL", (req, res) => {
+  urlDatabase[req.params.shortURL] = req.body.longURL; // Manali Bhattacharyya RA
+  res.redirect('/urls/');
+});
+
 app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
