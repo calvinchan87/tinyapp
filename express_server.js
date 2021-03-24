0f40093b73dd596fb46b97041fe9a8738ebdecbd
Calvin Chan <calvinchan87@gmail.com>
2021-03-24 02:29:17 +0000

    Create a Helper Functions Module

--- express_server.js
+++ express_server.js
@@ -15,34 +15,7 @@ app.use(cookieSession({
 
 app.set("view engine", "ejs");
 
-function generateRandomString() {
-  let randomSix   = '';
-  let chars       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-  let charsLength = chars.length;
-  for (var i = 0; i < 6; i++) {
-    randomSix += chars.charAt(Math.floor(Math.random() * charsLength));
-  }
-  return randomSix;
-};
-
-const getUserByEmail = function(input, database) {
-  for (const x in database) {
-    if (database[x].email === input) {
-      return database[x]; // refactoring ideas from @wesley-wong and @berk-ozer
-    }
-  }
-  return false;
-};
-
-const urlsForUser = function(input, database) {
-  let customURLs = {};
-  for (const x in database) {
-    if (database[x].userID === input) {
-      customURLs[x] = database[x];
-    }
-  }
-  return customURLs;
-};
+const { generateRandomString, getUserByEmail, urlsForUser } = require('./helpers');
 
 const urlDatabase = {
   // "b2xVn2": "http://www.lighthouselabs.ca",
